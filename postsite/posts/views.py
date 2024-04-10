0ec6307b8fa937fda828a63dfd6e36e137428817
negi20001211 <2000negi1@gmail.com>
2024-04-10 21:28:37 +0900

    編集ページの作成

--- postsite/posts/views.py
+++ postsite/posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,get_object_or_404
-from posts.models import Post 
+from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required
+from posts.models import Post
+from posts.forms import PostForm
 
 def top(request):
     return render(request,'posts/top.html')
@@ -9,9 +11,18 @@ def posts_top(request):
     context = {'posts':posts}
     return render(request,'posts/posts_top.html',context)
 
+@login_required
 def post_new(request):
-    return render(request,'posts/post_new.html')
-    
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.created_by = request.user
+            post.save()
+            return redirect('post_detail',post_id=post.pk) 
+    else:
+        form = PostForm()
+    return render(request,'posts/post_new.html',{'form':form})  
 
 def post_detail(request,post_id):
     post = get_object_or_404(Post,pk=post_id)
