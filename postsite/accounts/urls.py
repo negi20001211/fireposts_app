9ce825a282e50f181a3aa42aa32386323bf3ea60
negi20001211 <2000negi1@gmail.com>
2024-04-20 20:36:45 +0900

    研修カレンダーの作成

--- postsite/accounts/urls.py
+++ postsite/accounts/urls.py
@@ -3,12 +3,16 @@ from django.contrib.auth.views import LoginView,LogoutView
 from accounts import views
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
+from django.contrib.auth.models import User
 
 urlpatterns = [
+    path('signup/',CreateView.as_view(template_name='accounts/signup.html',
+                                      success_url='/',
+                                      form_class=UserCreationForm,
+                                      model=User
+                                      ),name='signup'),
     path('login/',LoginView.as_view(redirect_authenticated_user=True,
         template_name='accounts/login.html'),name='login'),
     path('logout/',LogoutView.as_view(),name='logout'),
-    path('signup/',CreateView.as_view(template_name='accounts/login.html'
-                                      ,success_url='/'
-                                      ,form_class=UserCreationForm),name='signup'),
+    
 ]
--- postsite/accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.models import Group
-
-def group_users(request, group_name):
-        group = Group.objects.get(name=group_name)
-        users_in_group = group.user_set.all()
-        return render(request, 'posts/posts_top.html', {'group': group, 'users_in_group': users_in_group})
